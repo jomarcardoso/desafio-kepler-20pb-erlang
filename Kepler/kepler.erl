ec5e0961d1e315e8ef48993987abded3034ef5f0
edhein <eric.dhein@hotmail.com>
2021-11-13 22:07:07 -0300

    new kepler version

--- Kepler/kepler.erl
+++ Kepler/kepler.erl
@@ -2,16 +2,21 @@
 -export([startKepler/1]).
 -import(produtor, [startProdutor/2]).
 
-loop() ->
+loop(State) ->
     receive
         {Type} ->
             String = "Atomo do tipo " ++ Type,
             io:format(String ++ " recebido.~n"),
-            loop()
+
+            Increment = fun(X) -> X + 1 end, 
+            NewState = maps:update_with(Type, Increment, 1, State),
+
+            io:format("~p~n", [maps:to_list(NewState)]),
+            loop(NewState)
     end.
 
 
 startKepler(Interval) ->
     spawn(produtor, startProdutor, [Interval, self()]),
     
-    loop().
+    loop(maps:new()).
