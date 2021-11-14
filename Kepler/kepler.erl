1cea7e7153240c6509ed1edd910dfc92b690847f
edhein <eric.dhein@hotmail.com>
2021-11-14 14:18:43 -0300

    adding molecule counter

--- Kepler/kepler.erl
+++ Kepler/kepler.erl
@@ -21,12 +21,14 @@ loop(State) ->
 
                     DecrementH = fun(H) -> H - 2 end,
                     DecrementO = fun(O) -> O - 1 end,
+                    Contador = fun(C) -> C + 1 end,
                     NewMapH = maps:update_with("H", DecrementH, NewState),
                     NewMapO = maps:update_with("O", DecrementO, NewMapH),
+                    NewMapContador = maps:update_with("Moleculas Formadas", Contador, 1, NewMapO),
 
-                    io:format("~p~n", [maps:to_list(NewMapO)]),
+                    io:format("~p~n", [maps:to_list(NewMapContador)]),
                     
-                    loop(NewMapO);
+                    loop(NewMapContador);
                 true ->
                     io:format("Ainda nao deu pra criar molecula.~n"),
                     loop(NewState)
@@ -37,7 +39,7 @@ verifyIfCanCreateMolecules(Map) ->
     Length = length(maps:keys(Map)),
 
     if
-        Length == 2 ->
+        Length >= 2 ->
             HydrogenNumber = maps:get("H", Map),
             OxygenNumber = maps:get("O", Map),
 
